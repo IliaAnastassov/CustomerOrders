ddd8655931c172754a8b4c9681290fe920b14b20
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-09-08 22:42:35 +0300

    Update WebAPI layer

---
+++ CustomerOrders.WebAPI/Controllers/CustomerController.cs
@@ -0,0 +1,28 @@
+﻿using CustomerOrders.Data;
+using CustomerOrders.Data.Interfaces;
+using CustomerOrders.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CustomerOrders.WebAPI.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        private readonly IRepository _repo = new DisconnectedRepository();
+
+        public Customer GetCustomer(string id)
+        {
+            return _repo.GetCustomer(id);
+        }
+
+        [Route("customer/{customerId}/orders")]
+        public IEnumerable<Order> GetOrdersByCustomerId(string customerId)
+        {
+            return _repo.GetOrdersByCustomerId(customerId);
+        }
+    }
+}
---
+++ CustomerOrders.WebAPI/Controllers/CustomersController.cs
@@ -0,0 +1,22 @@
+﻿using CustomerOrders.Data;
+using CustomerOrders.Data.Interfaces;
+using CustomerOrders.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CustomerOrders.WebAPI.Controllers
+{
+    public class CustomersController : ApiController
+    {
+        private readonly IRepository _repo = new DisconnectedRepository();
+
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return _repo.GetAllCustomers();
+        }
+    }
+}
