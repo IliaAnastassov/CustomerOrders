7541830f65cff4b907814938706dfda65acd2f32
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-09-09 23:09:31 +0300

    Update Home page to run with dummy data

--- CustomerOrders.Web/Controllers/HomeController.cs
+++ CustomerOrders.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CustomerOrders.Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,7 +11,17 @@ namespace CustomerOrders.Web.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var customers = new List<Customer>
+            {
+                new Customer { Id = 1, Name = "Shmuley Boteach", NumberOfOrders = 6 },
+                new Customer { Id = 2, Name = "Dildo Schwaggins", NumberOfOrders = 2 },
+                new Customer { Id = 3, Name = "Skank Hunt", NumberOfOrders = 4 },
+                new Customer { Id = 4, Name = "Palma Hayek", NumberOfOrders = 12 },
+                new Customer { Id = 5, Name = "John Doe", NumberOfOrders = 31 },
+                new Customer { Id = 6, Name = "Nignog Johnson", NumberOfOrders = 7 }
+            };
+
+            return View(customers);
         }
 
         public ActionResult About()
---
+++ CustomerOrders.Web/Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomerOrders.Web.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [Display(Name = "Number of Orders")]
+        public int NumberOfOrders { get; set; }
+    }
+}
