73c7020f79c5f95268883fed5ebbd5e0b9142558
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-09-09 14:38:12 +0300

    Update Repository

---
+++ CustomerOrders.Data/Repositories/Repository.cs
@@ -0,0 +1,33 @@
+﻿using CustomerOrders.Data.Interfaces;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace CustomerOrders.Data.Repositories
+{
+    public class Repository : IRepository
+    {
+        private readonly CustomerOrdersContext _context = new CustomerOrdersContext();
+
+        public IEnumerable<Customer> GetAllCustomers()
+        {
+            return _context.Customers.AsNoTracking()
+                                    .Include(c => c.Orders)
+                                    .OrderBy(c => c.ContactName)
+                                    .ToList();
+        }
+
+        public Customer GetCustomer(string id)
+        {
+            return _context.Customers.Find(id);
+        }
+
+        public IEnumerable<Order> GetOrdersByCustomerId(string id)
+        {
+            return _context.Orders.AsNoTracking()
+                                 .Include(o => o.Order_Details)
+                                 .Where(o => o.CustomerID == id)
+                                 .ToList();
+        }
+    }
+}
