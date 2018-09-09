2f499eac41d5cd7941007c84072aab83dea99a86
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-09-09 14:38:55 +0300

    Add models

---
+++ CustomerOrders.WebAPI/Models/CustomerDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CustomerOrders.WebAPI.Models
+{
+    public class CustomerDTO
+    {
+        public string CustomerID { get; set; }
+        public string ContactName { get; set; }
+        public ICollection<OrderDTO> Orders { get; set; }
+    }
+}
---
+++ CustomerOrders.WebAPI/Models/OrderDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CustomerOrders.WebAPI.Models
+{
+    public class OrderDTO
+    {
+        public int OrderID { get; set; }
+        public string CustomerID { get; set; }
+    }
+}
