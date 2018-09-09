ac000ed880f7aef4015e48fc4f74d9c146d14a8f
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-09-10 02:02:15 +0300

    Add models

---
+++ CustomerOrders.Web/ViewModels/HomeIndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomerOrders.Web.ViewModels
+{
+    public class HomeIndexViewModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        [Display(Name = "Number of Orders")]
+        public int NumberOfOrders { get; set; }
+    }
+}
