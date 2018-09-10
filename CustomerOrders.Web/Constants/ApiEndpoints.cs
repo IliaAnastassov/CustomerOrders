4fb0326316e94687dd344abd1778f6ec64308835
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-09-10 14:41:33 +0300

    Update API endpoints

--- CustomerOrders.Web/Constants/ApiEndpoints.cs
+++ CustomerOrders.Web/Constants/ApiEndpoints.cs
@@ -8,5 +8,7 @@ namespace CustomerOrders.Web.Constants
     public class ApiEndpoints
     {
         public const string Customers = "/api/customers";
+        public const string CustomerDetails = "/api/customer/{0}";
+        public const string CustomerOrders = "/api/customer/{0}/orders";
     }
 }
